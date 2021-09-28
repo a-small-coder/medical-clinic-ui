05e0741ed8c9da2251ed64545232a84e447e975e
master <hulktv222@mail.ru>
2021-09-28 11:03:12 +0300

    check

--- src/support_functions/api_requests.js
+++ src/support_functions/api_requests.js
@@ -86,17 +86,17 @@ export function standartGoodResponseHandler(response) {
 }
 
 export function standartErrorResponseHandler(err) {
-    if (err.response) {
+    if (err?.response) {
         // Request made and server responded
         console.log(err.response.data);
         console.log(err.response.status);
         console.log(err.response.headers);
-    } else if (err.request) {
+    } else if (err?.request) {
         // The request was made but no response was received
         console.log(err.request);
     } else {
         // Something happened in setting up the request that triggered an Error
-        console.log('Error', err.message);
+        console.log('Error', err?.message);
     }
 }
 
